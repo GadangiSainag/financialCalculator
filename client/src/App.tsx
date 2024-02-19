19e76c8b085f5bb813eaded25ec7b8c88939a186
Sainag Gadangi <saigadangi@gmail.com>
2024-02-20 01:35:24 +0530

    Understanding Redux with Input values

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "./state/counterSlice";
+import { RootState } from "./state/store";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {counterValue,totalAmount} = useSelector((state: RootState) => state.counter);
+  // const total = useSelector((state : RootState))
+  const dispatch = useDispatch();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>World's greatest Counter</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+       
+        <div className="inputSelector">
+          <input
+            className="sliderInput"
+            type="range"
+            name="sliderinput"
+            min={10}
+            max={50}
+            step={5}
+            value={counterValue}
+            onChange={(e) =>
+              dispatch(counterActions.increaseVal(Number(e.target.value)))
+            }
+          />
+          <input
+            className="boxInput"
+            type="number"
+            name="boxInput"
+            min={10}
+            max={50}
+            value={counterValue}
+            onChange={(e) =>
+              dispatch(counterActions.increaseVal(Number(e.target.value)))
+            }
+          />
+        </div>
+        <button>count is</button>
+        <h3>2 x input value = {totalAmount}</h3>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/state/counterSlice.ts
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counterValue: 0 , totalAmount :0},
+  reducers: {
+    increaseVal : (state,action) => {
+      state.counterValue = action.payload;
+      state.totalAmount = state.counterValue * 2
+    },
+
+    
+  },
+});
+
+export const counterActions = counterSlice.actions;
+export default counterSlice;
