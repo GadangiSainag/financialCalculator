82ac88ac5fdd4f0608f4cc5a8fad1c771ab53bb2
Sainag Gadangi <saigadangi@gmail.com>
2024-05-28 00:46:36 +0530

    Add dummy logo and Better navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,7 +1,9 @@
 import classes from "./NavBar.module.css";
+import logo from "../../public/favicon.svg"
 export default function NavBar() {
   return( 
     <div className={classes.navbar}>
+      <img src={logo} alt="Logo" className={classes.logo} />
   <div className={`${classes.header} ${classes.montserrat}`}>All useful calculators</div>
 
     </div>
